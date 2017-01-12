5a11b0ce7bcf896317f452d062776342d7ad9ab4
David Benicek <davidbenicek@hotmail.com>
2017-01-12 12:09:35 +0000

    Cleaning out .pyc files. Finishing chapter 3

--- project/rango/views.py
+++ project/rango/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+def about(request):
+    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/'>Index</a>")
